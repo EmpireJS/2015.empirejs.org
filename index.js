18471f3c13940810024a006ce86106feb6d6c025
indexzero <charlie.robbins@gmail.com>
2015-03-08 23:43:00 -0400

    [fix dist] Update package.json. Correct "paths" usage in `index.js`.

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ require('babel/polyfill')
 var Server = require('paradigm-server-express')
 
 var server = new Server({
+  paths: { routes: require('path').join(__dirname, 'server', 'routes') },
   port: 6060
 })
