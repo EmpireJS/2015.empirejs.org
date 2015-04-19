d4db7f2b3a501e91e9c89424a6e5adcc62bb7d06
indexzero <charlie.robbins@gmail.com>
2015-04-19 01:04:32 -0400

    [api fix] Simple redirect for CoC to Github. Fix minor typo in schedule.

--- server/routes.js
+++ server/routes.js
@@ -1,33 +1,15 @@
 module.exports = (server) => {
 
-  /*server.get('/about', (req, res) => {
-
-    res.render('about', {layout: 'layouts/main'})
-
+  server.get('/', (req, res) => {
+    res.render('index', {layout: 'layouts/main'})
   })
-
-  server.get('/contact', (req, res) => {
-
-    res.render('contact', {layout: 'layouts/main'})
-
-  })*/
 
   server.get('/scholarship', (req, res) => {
-
     res.render('scholarship', {layout: 'layouts/main'})
-
   })
 
-  /*server.get('/speakers', (req, res) => {
-
-    res.render('speakers', {layout: 'layouts/main'})
-
-  })*/
-
-  server.get('/', (req, res) => {
-
-    res.render('index', {layout: 'layouts/main'})
-
+  server.get('/code-of-conduct', (req, res) => {
+    res.redirect(301, 'https://github.com/empirejs/code-of-conduct');
   })
 
 }
